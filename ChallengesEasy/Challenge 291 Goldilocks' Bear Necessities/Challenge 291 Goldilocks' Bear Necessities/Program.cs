8f89de729dde425abdd54c802d288486460a5eaf
Tombarlow8 <Tombarlow8@gmail.com>
2017-06-27 21:05:34 +0100

    Challenge 291 Goldilocks Bear Necessities
    https://www.reddit.com/r/dailyprogrammer/comments/5bn0b7/20161107_challenge_291_easy_goldilocks_bear/

---
+++ ChallengesEasy/Challenge 291 Goldilocks' Bear Necessities/Challenge 291 Goldilocks' Bear Necessities/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Challenge_291_Goldilocks__Bear_Necessities
+{
+    class Program
+    {
+        /// <summary>
+        /// [2016-11-07] Challenge #291 [Easy] Goldilocks' Bear Necessities
+        /// https://www.reddit.com/r/dailyprogrammer/comments/5bn0b7/20161107_challenge_291_easy_goldilocks_bear/
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+            int[] chairWeight = { 100, 297, 66, 257, 276, 280, 219, 254, 86, 206, 71,104, 238, 52, 129, 144,
+                157, 210, 11, 268, 261, 12, 186, 174, 77, 54, 174, 16, 59, 290, 248 };
+ 
+            int[] porridgeTemp = { 120, 90, 110 ,113, 191, 129, 163, 193, 153 , 147, 137, 40, 127, 146, 197,
+                59, 124, 59, 54, 119, 121, 189, 108, 21 ,18 ,90 ,52 , 129, 181, 123, 132};
+                       
+            for(int i = 1; i < chairWeight.Length; i++)
+            {
+                if (chairWeight[i] >= chairWeight[0] && porridgeTemp[i] <= porridgeTemp[0])
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChallengesEasy/Challenge 291 Goldilocks' Bear Necessities/Challenge 291 Goldilocks' Bear Necessities/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Challenge 291 Goldilocks' Bear Necessities")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge 291 Goldilocks' Bear Necessities")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec146882-8af2-4b3f-8305-ec7e6f20da90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
