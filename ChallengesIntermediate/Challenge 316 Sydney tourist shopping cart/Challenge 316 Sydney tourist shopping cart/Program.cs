64670fb1d3566973893cd2dedd5f1b66b8e8a95f
Tombarlow8 <Tombarlow8@gmail.com>
2017-07-01 16:54:43 +0100

    Challenge #316 Sydney tourist shopping cart
    https://www.reddit.com/r/dailyprogrammer/comments/6d29om/20170524_challenge_316_intermediate_sydney/

---
+++ ChallengesIntermediate/Challenge 316 Sydney tourist shopping cart/Challenge 316 Sydney tourist shopping cart/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Challenge_316_Sydney_tourist_shopping_cart
+{
+    class Program
+    {
+        /// <summary>
+        /// [2017-05-24] Challenge #316 [Intermediate] Sydney tourist shopping cart
+        /// https://www.reddit.com/r/dailyprogrammer/comments/6d29om/20170524_challenge_316_intermediate_sydney/
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            string[] order1 = { "OH", "OH", "OH", "BC","SK" };
+            string[] order2 = { "OH", "BC", "BC", "SK", "SK" };
+            string[] order3 = { "BC", "BC", "BC", "BC", "BC","BC", "OH", "OH" };
+            string[] order4 = { "SK", "SK", "BC" };
+
+            string[][] orders = { order1, order2, order3, order4 };
+
+            ShoppingCart Sp = new ShoppingCart();
+
+            foreach (var order in orders)
+            {
+                Sp.GetOrder(order);
+                Sp.ViewCart();
+                Console.WriteLine("Total = " + Sp.OrderTotal(order));
+                Sp.EmpytCart();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ChallengesIntermediate/Challenge 316 Sydney tourist shopping cart/Challenge 316 Sydney tourist shopping cart/ShoppingCart.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challenge_316_Sydney_tourist_shopping_cart
+{
+    class ShoppingCart
+    {
+        List<string> cartList = new List<string>();
+
+        public void GetOrder(string[] order)
+        {
+            foreach (string item in order)
+            {
+                cartList.Add(item);
+            }
+        }
+
+        public void ViewCart()
+        {
+            int itemNumber = 1;
+
+            foreach (string item in cartList)
+            {
+                Console.WriteLine("{0}: {1}", itemNumber, item);
+                itemNumber++;
+            }
+        }
+
+        public int OrderTotal(string[] order)
+        {
+            //OH Opera house tour	$300.00
+            //BC Sydney Bridge Climb	$110.00
+            //SK Sydney Sky Tower	$30.00
+
+            int orderTotal = 0;
+            int OHcount = 0;
+            int BCcount = 0;
+            int SKcount = 0;
+            int OHprice = 300;
+            int BCprice = 110;
+            int SKprice = 30;
+
+            foreach (string item in cartList)
+            {
+                if (item == "OH")
+                {
+                    orderTotal += OHprice;
+                    OHcount++;
+                }
+                if (item == "BC")
+                {
+                    orderTotal += BCprice;
+                    BCcount++;
+                }
+                if (item == "SK")
+                {
+                    orderTotal += SKprice;
+                    SKcount++;
+                }
+            }
+
+            //applying discount
+
+            if (OHcount % 3 == 0)
+            {
+                //We are going to have a 3 for 2 deal on opera house ticket.For example,
+                //if you buy 3 tickets, you will pay the price of 2 only getting another one completely 
+                //free of charge.
+                orderTotal -= (OHprice * (OHcount / 3));
+            }
+            if (SKcount > OHcount && SKcount != 0 && OHcount != 0)
+            {                
+                //We are going to give a free Sky Tower tour for with every Opera House tour sold
+                orderTotal -= (SKprice * (SKcount -= OHcount));                
+            }
+            if (OHcount > SKcount && SKcount != 0 && OHcount != 0)
+            {
+                //We are going to give a free Sky Tower tour for with every Opera House tour sold
+                orderTotal -= (SKprice * SKcount);
+            }
+            if (BCcount >= 4)
+            {
+                //The Sydney Bridge Climb will have a bulk discount applied, where the price will drop $20, 
+                //if someone buys more than 4
+                orderTotal -= 20 * BCcount;
+            }
+
+            return orderTotal;
+        }
+
+        public void EmpytCart()
+        {
+            cartList.Clear();
+        }
+    }
+}
